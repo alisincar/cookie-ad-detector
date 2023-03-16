e73de9cd6b709522f35d3a814fce8b3aca0df953
alisincar <ali@mozared.com>
2023-03-16 12:00:12 +0300

    ad detector, cookie manager

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  let tumCerezleriSil=document.getElementById("tum-cerezleri-sil");
+  tumCerezleriSil.addEventListener('click', removeAllCookieListener());
+
   // Reklam sayısını al ve popup.html'deki ilgili yerlere yerleştir
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+
+    var tabId = tabs[0].id;
     chrome.cookies.getAllCookieStores(function(cookieStores) {
       console.log("cookieStores",cookieStores);
     });
@@ -67,22 +73,22 @@ chrome.webNavigation.getAllFrames({
 */
 
 
-var tabId = tabs[0].id;
+let cookieList = document.getElementById('cookieList');
   chrome.cookies.getAllCookieStores(function(cookiestores) {
-    var cookieStore = cookiestores.find(function(obj) {
+    let cookieStore = cookiestores.find(function(obj) {
       return obj.tabIds.includes(tabId);
     });
     chrome.cookies.getAll({ "storeId": cookieStore.id }, function(cookies) {
       console.log("cookiestr",JSON.stringify(cookies));
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        console.log("cookie",cookie);
-        var li = document.createElement('li');
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let li = document.createElement('li');
+        li.setAttribute("id",cookie.domain+ cookie.path+'-'+ cookie.name);
         li.innerHTML = cookie.domain+'-'+ cookie.name + ' : ' + cookie.value;
         
-        var button = document.createElement('button');
+        let button = document.createElement('button');
         button.innerText = 'Sil';
-        
+
         button.addEventListener('click', createRemoveCookieListener(cookie));
         
         li.appendChild(button);
@@ -91,19 +97,19 @@ var tabId = tabs[0].id;
     });
   });
     chrome.tabs.sendMessage(tabs[0].id, { action: 'getAdCount' }, response => {
-      if (chrome.runtime.lastError) {
+     /* if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError.message);
         return;
       }
-  
+  */
       if (response && response.adCount) {
         document.getElementById('adCount').innerText = response.adCount;
   
         if (response.adCount === 0) {
-          document.getElementById('status').innerText = 'Reklam yok.';
+          document.getElementById('status').innerText = 'No ads.';
           document.getElementById('status').style.color = 'green';
         } else {
-          document.getElementById('status').innerText = 'Reklam bulundu.';
+          document.getElementById('status').innerText = 'Ad found.';
           document.getElementById('status').style.color = 'red';
         }
       }
@@ -112,13 +118,28 @@ var tabId = tabs[0].id;
 });
 
 
+
 function createRemoveCookieListener(cookie) {
   return function() {
+    console.log("cookie",cookie);
     chrome.cookies.remove({
-      url: cookie.url,
+      url: "https://" +cookie.domain+ cookie.path,
       name: cookie.name
     }, function() {
-      alert('Çerez silindi:', cookie.name);
+      document.getElementById(cookie.domain+ cookie.path+'-'+ cookie.name).remove()
     });
   };
 }
+
+function removeAllCookieListener() {
+  return function() {
+    chrome.cookies.getAll({}, function(cookies) {
+      console.log(cookies);
+      for(var i=0; i<cookies.length;i++) {
+        document.getElementById(cookies[i].domain+ cookies[i].path+'-'+ cookies[i].name).remove();
+        chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
+      }
+    });
+  };
+}
+
